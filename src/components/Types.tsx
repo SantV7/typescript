bce097515d62251fe5514fe22df3065688887aa1
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-04-20 00:29:23 -0300

    Criando as tipagens string e number

---
+++ src/components/Types.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+const Types = () => {
+    const taxJuros: number = 4.3
+    const [text, setText]= useState<string>('')
+
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Types
