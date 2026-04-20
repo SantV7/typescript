32b38af989531d6909330645b1888952b8e66abd
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-04-20 00:44:43 -0300

    Fiz boolean, number e string, faltam alguns, que vou fazer depois ....

--- src/components/Types.tsx
+++ src/components/Types.tsx
@@ -2,11 +2,19 @@ import { useState } from "react"
 
 const Types = () => {
     const taxJuros: number = 4.3
-    const [text, setText]= useState<string>('')
+    const [text] = useState<string>('')
+    const valorFolga: boolean = true
 
 
   return (
     <>
+    <h1>Testando alguns tipos</h1>
+    <p>valor da taxa: {taxJuros > 0 ? taxJuros : 'nenhuma'}</p>
+
+    <p>{valorFolga === true ? 'Teremos folga' : 'trabahhar q nem escravo'}</p>
+    
+
+    <p>{text}</p>
     
     </>
   )
