5d493a4be2e6cf54e948902c225a5133b9b7fed9
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-04-25 12:12:52 -0300

    Criação da seção para  os  tipos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import Types from "./components/Types"
+
 function App() {
 
 
   return (
     <>
     <h1>Jornada de TypeScript - TS</h1>
-     
+    <section>
+      <h1>Tipagem de váriaveis</h1>
+      <Types />
+    </section>
     </>
   )
 }
--- src/components/Types.tsx
+++ src/components/Types.tsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 const Types = () => {
     const taxJuros: number = 4.3
     const [text] = useState<string>('')
-    const valorFolga: boolean = true
+    const folga: boolean = true
 
 
   return (
@@ -11,7 +11,7 @@ const Types = () => {
     <h1>Testando alguns tipos</h1>
     <p>valor da taxa: {taxJuros > 0 ? taxJuros : 'nenhuma'}</p>
 
-    <p>{valorFolga === true ? 'Teremos folga' : 'trabahhar q nem escravo'}</p>
+    <p>{folga === true ? 'Teremos folga' : 'trabahhar q nem escravo'}</p>
     
 
     <p>{text}</p>
